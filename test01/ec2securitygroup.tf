1a3b1e5c3ae2062e4a59b2279b4932bf7d1fbf3f
imdjango <63759249+imdjango@users.noreply.github.com>
2020-05-25 15:59:41 +0900

    사무실 IP 변경

--- test01/ec2securitygroup.tf
+++ test01/ec2securitygroup.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "imdjango" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["15.165.205.209/32"]
   }
 
   ingress {
